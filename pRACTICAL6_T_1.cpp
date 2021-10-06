35c91f6aadc7a525b1fbb9e901a1d7cfc2c99a02
snehanegi25 <91174457+snehanegi25@users.noreply.github.com>
2021-10-06 09:59:59 +0530

    Add files via upload

---
+++ pRACTICAL6_T_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<string>
+
+using namespace std;
+int main()
+{
+int i=0,n=1,sum;
+sum = i+n;
+cout<<sum<<endl;
+string a="ABC";
+string s = i + a;
+cout<<s<<endl;
+return 0;
+}
+
